9f529da1e9a7f626db28042b752ac313dfa717de
bee <jc2024@kent.ac.uk>
2022-12-10 23:11:58 +0000

    timed step

--- pyrosim/files/simulate.py
+++ pyrosim/files/simulate.py
@@ -1,1 +1,15 @@
-pass
+import sys
+import time
+sys.path.insert(0, '..')
+import pybullet as p
+
+# client handles physics and draws to GUI
+physicsClient = p.connect(p.GUI)
+
+for i in range(1000):
+    p.stepSimulation()
+    print(i)
+    time.sleep(1/60)
+
+
+p.disconnect()
--- pyrosim/files/test.py
+++ pyrosim/files/test.py
@@ -3,6 +3,5 @@ sys.path.insert(0, '..')
 import pyrosim #noqa
 
 sim = pyrosim.Simulator()
-
 sim.start()
 sim.wait_to_finish()
