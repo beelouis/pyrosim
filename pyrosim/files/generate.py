b0165777ed59539dbfe8e2c5b50c240437021c66
bee <jc2024@kent.ac.uk>
2022-12-11 01:48:10 +0000

    got stacks of boxes working

--- pyrosim/files/generate.py
+++ pyrosim/files/generate.py
@@ -2,6 +2,24 @@ import sys
 sys.path.insert(0, "../pyrosim/pyrosim")
 import pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+
+def tower(x, y, z, length, width, height):
+    for i in range(10):
+        pyrosim.Send_Cube(name=(f"Box{i}"), pos=[x, y, z] , size=[length, width, height])
+        z += height
+        length *= .9
+        width *= .9
+        height *=.9
+
+length, width, height = (.5, .5, .5)
+x, y, z = (0, 0, height/2)
+
+for X in range(5):
+    for Y in range(5):
+        tower(x, y, z, length, width, height)
+        y += width
+    y = 0
+    x += length
+
 pyrosim.End()
--- pyrosim/files/simulate.py
+++ pyrosim/files/simulate.py
@@ -2,11 +2,16 @@ import sys
 import time
 sys.path.insert(0, '..')
 import pybullet as p
+import pybullet_data
 
 # client handles physics and draws to GUI
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.loadSDF("box.sdf")
+p.setGravity(0, 0, -9.8)
+#plane comes with pybullet
+planeID = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     p.stepSimulation()
